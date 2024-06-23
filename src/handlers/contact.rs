3c0f10048fabedeb432c9222fa1d91c1a77402e4
Pablo1Gustavo <pablo1gustavo.dev@gmail.com>
2024-06-23 20:51:32 -0300

    refactor: fix and refactor contact handlers

--- src/handlers/contact.rs
+++ src/handlers/contact.rs
@@ -5,35 +5,41 @@ use serde_json::{json, Value};
 use sqlx::Type;
 
 #[derive(Debug, Serialize, Deserialize, Type)]
+#[serde(rename_all = "lowercase")]
 #[sqlx(type_name = "contact_type", rename_all = "lowercase")]
-pub enum ContactType {
+pub enum ContactType
+{
     Email,
     Phone,
 }
 
 #[derive(Debug, FromRow, Serialize)]
-pub struct UserContact {
-    pub id: i64,
-    pub user_id: i64,
-    pub contact: String,
+pub struct UserContact
+{
+    pub id          : i64,
+    pub user_id     : i64,
+    pub contact     : String,
     pub contact_type: String,
 }
 
 #[derive(Deserialize)]
-pub struct CreateContactRequest {
+pub struct CreateContactRequest
+{
     pub user_id: i64,
     pub contact: String,
-    pub contact_type: ContactType,
+    pub r#type : ContactType,
 }
+
 pub async fn create_contact(
     State(db_pool): State<PgPool>,
     Json(req): Json<CreateContactRequest>,
-) -> Result<(StatusCode, Json<Value>), (StatusCode, Json<Value>)> {
+) -> Result<(StatusCode, Json<Value>), (StatusCode, Json<Value>)>
+{
     let result = sqlx::query!(
         "INSERT INTO users_contacts (user_id, contact, type) VALUES ($1, $2, $3) RETURNING id",
         req.user_id,
         req.contact,
-        req.contact_type as ContactType
+        req.r#type as ContactType
     )
     .fetch_one(&db_pool)
     .await
@@ -58,7 +64,8 @@ pub async fn create_contact(
 pub async fn delete_contact(
     State(db_pool): State<PgPool>,
     Path(id): Path<i64>,
-) -> Result<(StatusCode, Json<Value>), (StatusCode, Json<Value>)> {
+) -> Result<(StatusCode, Json<Value>), (StatusCode, Json<Value>)>
+{
     let result = sqlx::query!("DELETE FROM users_contacts WHERE id = $1", id)
         .execute(&db_pool)
         .await
@@ -70,17 +77,15 @@ pub async fn delete_contact(
             })),
         ))?;
 
-    match result.rows_affected() {
+    match result.rows_affected()
+    {
         0 => Err((
             StatusCode::NOT_FOUND,
             Json(json!({"message": "Contact not found."})),
         )),
         _ => Ok((
             StatusCode::OK,
-            Json(json!({
-                "status": "success",
-                "message": "Contact deleted successfully.",
-            })),
+            Json(json!({"message": "Contact deleted successfully.",})),
         )),
     }
 }
@@ -88,20 +93,20 @@ pub async fn delete_contact(
 pub async fn list_contacts(
     State(db_pool): State<PgPool>,
     Path(user_id): Path<i64>,
-) -> Result<(StatusCode, Json<Vec<UserContact>>), (StatusCode, Json<Value>)> {
-    let contacts = sqlx::query_as::<_, UserContact>(
-        "SELECT * FROM users_contacts WHERE user_id = $1"
-    )
-    .bind(user_id)
-    .fetch_all(&db_pool)
-    .await
-    .map_err(|e| (
-        StatusCode::INTERNAL_SERVER_ERROR,
-        Json(json!({
-            "status": "error",
-            "message": e.to_string()
-        })),
-    ))?;
+) -> Result<(StatusCode, Json<Vec<UserContact>>), (StatusCode, Json<Value>)>
+{
+    let contacts = sqlx::query_as::<_, UserContact>
+        ("SELECT * FROM users_contacts WHERE user_id = $1")
+        .bind(user_id)
+        .fetch_all(&db_pool)
+        .await
+        .map_err(|e| (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({
+                "status": "error",
+                "message": e.to_string()
+            })),
+        ))?;
 
     Ok((StatusCode::OK, Json(contacts)))
 }
